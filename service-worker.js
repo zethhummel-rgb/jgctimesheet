80bd65e5eb7cfd7347a6fe99942ee9f55b38d212
zethhummel-rgb <zethhummel@gmail.com>
2026-06-20 08:25:13 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const JGC_CACHE_NAME = "jgc-portal-v310";
+const JGC_CACHE_NAME = "jgc-portal-v311";
 const JGC_APP_SHELL = [
   "./",
   "./index.html",
@@ -40,8 +40,8 @@ const JGC_APP_SHELL = [
   "./styles.css?v=2",
   "./common.js?v=3",
   "./field-calculator.css?v=7",
-  "./calculator-engine.js?v=15",
-  "./calculator-functions.js?v=15",
+  "./calculator-engine.js?v=16",
+  "./calculator-functions.js?v=16",
   "./field-calculator.js?v=12",
   "./auth.js",
   "./inspection-records.js",
