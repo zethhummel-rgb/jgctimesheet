913e809b63e49b2409ed5dc58c9d8b84a75d1e89
zethhummel-rgb <zethhummel@gmail.com>
2026-06-21 07:54:55 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const JGC_CACHE_NAME = "jgc-portal-v330";
+const JGC_CACHE_NAME = "jgc-portal-v331";
 const JGC_APP_SHELL = [
   "./",
   "./index.html",
@@ -39,7 +39,7 @@ const JGC_APP_SHELL = [
   "./tele-handler.html",
   "./styles.css?v=2",
   "./common.js?v=3",
-  "./field-calculator.css?v=12",
+  "./field-calculator.css?v=13",
   "./calculator-engine.js?v=25",
   "./calculator-functions.js?v=28",
   "./field-calculator.js?v=25",
