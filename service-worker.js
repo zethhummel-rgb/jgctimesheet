3e355c840362589bcf53d0cce18a74ff2fecf9ef
zethhummel-rgb <zethhummel@gmail.com>
2026-07-13 23:16:17 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const JGC_CACHE_NAME = "jgc-portal-v509";
+const JGC_CACHE_NAME = "jgc-portal-v510";
 const JGC_APP_SHELL = [
   "./",
   "./index.html",
@@ -51,7 +51,7 @@ const JGC_APP_SHELL = [
   "./common.js?v=22",
   "./purchase-orders.css?v=7",
   "./purchase-orders-pdf.js?v=1",
-  "./purchase-orders.js?v=9",
+  "./purchase-orders.js?v=10",
   "./purchase-orders-admin.js?v=6",
   "./work-order-digital-pos.js?v=1",
   "./safety-acknowledgements.js?v=3",
