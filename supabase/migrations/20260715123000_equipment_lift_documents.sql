6ff4491d6cfcc0319e42c6effaee3414a72c2f47
zethhummel-rgb <zethhummel@gmail.com>
2026-07-15 15:10:12 -0400

    Update 20260715123000_equipment_lift_documents.sql

--- supabase/migrations/20260715123000_equipment_lift_documents.sql
+++ supabase/migrations/20260715123000_equipment_lift_documents.sql
@@ -106,7 +106,7 @@ for select
 to anon, authenticated
 using (
   bucket_id = 'equipment-documents'
-  and storage.allow_any_operation(array['object.get_authenticated_info', 'object.get_authenticated'])
+  and storage.allow_any_operation(array['object.get_authenticated_info', 'object.get_authenticated', 'object.sign'])
   and jgc_private.equipment_document_storage_can_read(name)
 );
 
