265476da81e1c9855c3381ebe282d496fc1d3955
zethhummel-rgb <zethhummel@gmail.com>
2026-07-17 10:03:07 -0400

    Acknowledgement Design Update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const JGC_CACHE_NAME = "jgc-portal-v555";
+const JGC_CACHE_NAME = "jgc-portal-v556";
 const JGC_APP_SHELL = [
   "./",
   "./index.html",
@@ -61,6 +61,7 @@ const JGC_APP_SHELL = [
   "./home-design-system.css?v=1",
   "./field-calculator-design-system.css?v=1",
   "./notification-settings-design-system.css?v=1",
+  "./acknowledgement-design-system.css?v=1",
   "./common.js?v=24",
   "./purchase-orders.css?v=15",
   "./purchase-orders-pdf.js?v=1",
