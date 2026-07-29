8c14c2bc02994248de83ad209e6ac0de7a8a34f7
zethhummel-rgb <zethhummel@gmail.com>
2026-07-29 10:09:58 -0400

    WO Update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const JGC_RELEASE_ID = "647";
+const JGC_RELEASE_ID = "648";
 const JGC_CACHE_PREFIX = "jgc-portal-v";
 const JGC_CACHE_NAME = JGC_CACHE_PREFIX + JGC_RELEASE_ID;
 const JGC_APP_SHELL = [
