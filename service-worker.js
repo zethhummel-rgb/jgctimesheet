240565b9f19f4fcf92a6a3a3db0c36ba3b2bcc2d
zethhummel-rgb <zethhummel@gmail.com>
2026-08-21 09:50:13 -0400

    Prefix internal PDF download names

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const JGC_RELEASE_ID = "733";
+const JGC_RELEASE_ID = "734";
 const JGC_CACHE_PREFIX = "jgc-portal-v";
 const JGC_CACHE_NAME = JGC_CACHE_PREFIX + JGC_RELEASE_ID;
 const JGC_APP_SHELL = [
@@ -133,14 +133,14 @@ const JGC_APP_SHELL = [
   "./vendor/jszip.min.js?v=1",
   "./vendor/jspdf.umd.min.js?v=1",
   "./vendor/lucide.min.js",
-  "./estimating/assets/index-B-NEpCct.js",
+  "./estimating/assets/index-BskKTkan.js",
   "./estimating/assets/index-CSpT8_RI.css",
-  "./estimating/assets/es-CVSmfeHf.js",
-  "./estimating/assets/pdf-DIAC1Pb6.js",
-  "./estimating/assets/src-C4kz5Hln.js",
-  "./estimating/assets/proposal-pdf-CRaVSrwr.js",
-  "./estimating/assets/purchase-order-pdf-Xqhn-wPD.js",
-  "./estimating/assets/quote-backup-pdf-CtT0VG7f.js",
+  "./estimating/assets/es-DMwt50Hh.js",
+  "./estimating/assets/pdf-BipCga4G.js",
+  "./estimating/assets/src-DQMGlm4r.js",
+  "./estimating/assets/proposal-pdf-3lGYcbq4.js",
+  "./estimating/assets/purchase-order-pdf-U9LESapU.js",
+  "./estimating/assets/quote-backup-pdf-DmR7XdD-.js",
   "./estimating/jgc-letterhead-logo.jpg",
   "./estimating/jgc-logo-transparent.png",
   "./logo.webp",
