f7b64c886ada996c2ab3b1e845ac6b9547700023
zethhummel-rgb <zethhummel@gmail.com>
2026-09-08 14:17:04 -0400

    Reduce desktop job list row height by forty percent

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
-const JGC_RELEASE_ID = "870";
+const JGC_RELEASE_ID = "871";
 const JGC_CACHE_PREFIX = "jgc-portal-v";
 const JGC_CACHE_NAME = JGC_CACHE_PREFIX + JGC_RELEASE_ID;
 const JGC_APP_SHELL = [
-  "./estimating/assets/index-4hRHU-Nn.css",
-  "./estimating/assets/index-B4N5vm5i.js",
+  "./estimating/assets/index-B9ujJWYE.css",
+  "./estimating/assets/index-o6-u8dMD.js",
   "./estimating/assets/job-accounting-workbook-NRWhLfpj.js",
-  "./estimating/assets/purchase-order-pdf--cXE_oCb.js",
-  "./estimating/assets/proposal-pdf-BhXLnZpi.js",
-  "./estimating/assets/src-BCpjQOA4.js",
-  "./estimating/assets/quote-backup-pdf-A9s7Z5kk.js",
-  "./estimating/assets/es-79XTbb7z.js",
-  "./estimating/assets/pdf-DqEfboAr.js",
+  "./estimating/assets/purchase-order-pdf-DLmm14rb.js",
+  "./estimating/assets/proposal-pdf-FKOQGuS6.js",
+  "./estimating/assets/src-BU83_Qpn.js",
+  "./estimating/assets/quote-backup-pdf-CZeaQSO3.js",
+  "./estimating/assets/es-yBYLNEJ7.js",
+  "./estimating/assets/pdf-BwAmWHP3.js",
   "./",
   "./index.html",
   "./acknowledge.html",
