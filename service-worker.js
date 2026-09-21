635f1e22a25246c72535d51e63bf9489b71fdbfa
zethhummel-rgb <zethhummel@gmail.com>
2026-09-21 07:35:35 -0400

    Add Open Job List shortcut to admin summary

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const JGC_RELEASE_ID = "899";
+const JGC_RELEASE_ID = "900";
 const JGC_CACHE_PREFIX = "jgc-portal-v";
 const JGC_CACHE_NAME = JGC_CACHE_PREFIX + JGC_RELEASE_ID;
 const JGC_APP_SHELL = [
