6de0a7e9c880aa9b5e9d45785e415efde06188e0
zethhummel-rgb <zethhummel@gmail.com>
2026-09-25 12:35:13 -0400

    WIP: compact logo beside Home; hide page-top logo on screen (release 918)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const JGC_RELEASE_ID = "917";
+const JGC_RELEASE_ID = "918";
 const JGC_CACHE_PREFIX = "jgc-portal-v";
 const JGC_CACHE_NAME = JGC_CACHE_PREFIX + JGC_RELEASE_ID;
 const JGC_APP_SHELL = [
@@ -335,7 +335,7 @@ const JGC_APP_SHELL = [
   "./notification-settings-design-system.css?v=3",
   "./acknowledgement-design-system.css?v=2",
   "./login-design-system.css?v=3",
-  "./common.js?v=49",
+  "./common.js?v=50",
   "./admin-global-search.js?v=8",
   "./accounting-workbook.js?v=9",
   "./accounting-admin.js?v=14",
